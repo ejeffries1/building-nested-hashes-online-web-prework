49a5930e5682101e4ae24f397e66f14d01a89ea5
Evan Jeffries <ejeffries14@gmail.com>
2019-07-10 08:02:04 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,6 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        friend_1 = {:name => "Steven", :age => "30", :attitude => "confused"}
+        friend_2 = {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
