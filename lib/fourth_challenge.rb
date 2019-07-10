74ccd6ddf3937aab75a5cbecf60189744e27e135
Evan Jeffries <ejeffries14@gmail.com>
2019-07-10 08:22:31 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        {name: "Benvolio", age: "17", attitude: "worried"}
+        {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
@@ -16,7 +16,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        :friend_1 => {:name => "Steven", :age => "30", :attitude => "confused"}
+        :friend_1 => {:name => "Steven", :age => "30", :attitude => "confused"},
         :friend_2 => {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
